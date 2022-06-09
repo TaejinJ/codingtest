ba3fa03326097f6c3498f514edb10d0d51320ac9
TaeJin Jo <90677238+TaejinJ@users.noreply.github.com>
2022-06-09 15:33:56 +0900

    Add files via upload

---
+++ quest1.java
@@ -0,0 +1,14 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class quest1 {
+
+	public static void main(String[] args) {
+		Scanner sc  = new Scanner(System.in);
+		String id= sc.nextLine();
+		System.out.println(id+"??");		
+	}
+
+}
+	
