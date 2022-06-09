1d94bcea374d8a63468ac518312d35c05b86d2be
TaeJin Jo <90677238+TaejinJ@users.noreply.github.com>
2022-06-09 15:34:27 +0900

    Add files via upload
    백준 18108문제

---
+++ quest2.java
@@ -0,0 +1,15 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class quest2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int year= sc.nextInt();
+		System.out.println(year-543);
+	
+
+	}
+
+}
